d0a5d1241464e7a84e5ad74fe2a3e801088b6a93
taterntots <matthew.a.weidner@gmail.com>
2019-10-15 16:35:58 -0400

    solved three out of the five array-method assignments

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,21 +58,47 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+
+runners.forEach(function(names) {
+  fullNames.push(`${names.first_name} ${names.last_name}`)
+})
+
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+
+runners.map(function(capNames){
+  return firstNamesAllCaps.push(capNames.first_name.toUpperCase());
+})
+
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+
+runners.filter(function(size){
+  return runnersLargeSizeShirt.push(size.shirt_size.toString());
+})
+
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+// let ticketPriceTotal = 0;
+
+// runners.reduce(function(accumulator, currentValue){
+//   return ticketPriceTotal.push(accumulator + currentValue.donation);
+// }, 0);
+
+// console.log(ticketPriceTotal);
+
+const ticketPriceTotal = runners.reduce(function(accumulator, currentValue){
+  return accumulator + currentValue.donation;
+}, 0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
